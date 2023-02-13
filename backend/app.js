0f84f260d61b45d5a516147255acf78e76b5dec3
Biseray <allan.biseray@outlook.fr>
2023-02-13 16:15:27 +0100

    creation des routes sinup et login , (ok) affichage produit (ok) routes post, get(pour un produit precis),  put créé mais ne fonctionne pas encore

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const saucesRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+ const mongooseConnect =  require('./mongoose');
+mongoose.connect(mongooseConnect.mongooselog,
+
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
- });
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.use('/api/sauces', saucesRoutes);
+ app.use('/api/auth', userRoutes);
+ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
+
---
+++ backend/models/thing.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const thingSchema = mongoose.Schema({
+    userId: {  type: String, require: true },
+    name: { type: String, require: true },
+    manufacturer: { type: String, require: true },
+    description: { type: String, require: true },
+    mainPepper: { type: String, require: true },
+    imageUrl: { type: String, require: true },
+    heat: { type: Number, require: true },
+    // likes: { type: Number, require: true },
+    // dislikes: { type: Number, require: true },
+    // userdislikes: { type: String, require: true },
+    // usersLiked: { type: String, require: true },
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
+
+
