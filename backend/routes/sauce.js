1c2b461a4d356b4b413356b736335a1b8569d070
Biseray <allan.biseray@outlook.fr>
2023-05-15 11:26:59 +0200

    merge

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const stuffCtrl = require('../controllers/sauces');
+const Ctrl = require('../controllers/sauces');
 
 const auth = require('../middleware/auth');
 
@@ -12,11 +12,11 @@ const multer = require('../middleware/multer-config');
 
 
 
-router.get('/', auth,  stuffCtrl.getAllSauce); //afficher tout les sauces de la page daccueil 
-router.get('/:id', auth, stuffCtrl.getOneSauce); // afficher une sauce 
-router.post('/', auth, multer, stuffCtrl.createSauce); //cree une sauce
-router.delete('/:id', auth, stuffCtrl.deleteSauce); // supprimer une sauce 
-router.put('/:id', auth, multer, stuffCtrl.modifySauce); //modifier le sauce
- router.post('/:id/like', auth, stuffCtrl.createLike);
+router.get('/', auth,  Ctrl.getAllSauce); //afficher tout les sauces de la page daccueil 
+router.get('/:id', auth, Ctrl.getOneSauce); // afficher une sauce 
+router.post('/', auth, multer, Ctrl.createSauce); //cree une sauce
+router.delete('/:id', auth, Ctrl.deleteSauce); // supprimer une sauce 
+router.put('/:id', auth, multer, Ctrl.modifySauce); //modifier le sauce
+router.post('/:id/like', auth, Ctrl.createLike);
 
 module.exports = router;
